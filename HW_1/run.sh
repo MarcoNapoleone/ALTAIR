1acc9e43dcf9375c5fce500880ca165bc1a5fc9e
Marco Napoleone <napoleonemarco25@gmail.com>
2024-10-21 23:58:25 +0200

    v0.1.4 - figs export

--- HW_1/run.sh
+++ HW_1/run.sh
@@ -2,7 +2,7 @@
 source ./venv/bin/activate
 pip install -r requirements.txt
 
-python ./scripts/scraper.py "Recommender systems" 50
+python ./scripts/scraper.py "Recommender systems" 500
 python ./scripts/parser.py ./sources/ ./extraction/
 python ./scripts/json2csv.py ./extraction/ ./dataset/tables.csv
 
