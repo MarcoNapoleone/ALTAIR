3dadac42d0d896e091a69b05539b4ca99ccadfd0
marco <napoleonemarco25@gmail.com>
2024-12-03 16:29:08 +0100

    CORS

--- search_engine/src/main/java/it/uniroma3/idd/search_engine/config/WebConfig.java
+++ search_engine/src/main/java/it/uniroma3/idd/search_engine/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Applica CORS a tutti gli endpoint
-                               .allowedOrigins("http://localhost", "https://altair.marconapoleone.me", "https://altair-search.web.app") // Domini consentiti
-                               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Metodi consentiti
-                               .allowedHeaders("*"); // Intestazioni consentite
+                .allowedOrigins("http://localhost/*", "https://altair.marconapoleone.me/*", "https://altair-search.web.app/*") // Domini consentiti
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Metodi consentiti
+                .allowedHeaders("*"); // Intestazioni consentite
     }
 }
